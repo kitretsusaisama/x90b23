405989c08747e1cb457c42b2f9038cc5296a4d75
Densuke Alpha <98268964+kitretsusaisama@users.noreply.github.com>
2022-05-25 12:56:41 +0530

    adding url in links

--- pages/_document.js
+++ pages/_document.js
@@ -16,12 +16,12 @@ class MyDocument extends Document {
 <meta name='robots' content='max-image-preview:large' />
 <link rel='dns-prefetch' href='https://fonts.googleapis.com/' />
 <link rel='dns-prefetch' href='https://s.w.org/' />
-<link rel='stylesheet' id='smartmag-core-style'  href='http://localhost:3000/styles/main.css' type='text/style' media='all' />
+<link rel='stylesheet' id='smartmag-core-style'  href='https://x90b23.vercel.app/styles/main.css' type='text/style' media='all' />
 <link crossorigin="anonymous" rel='stylesheet' id='smartmag-fonts-style'  href='https://fonts.googleapis.com/style?family=Public+Sans%3A400%2C400i%2C500%2C600%2C700' type='text/style' media='all' />
-<link rel='stylesheet' id='magnific-popup-style'  href='http://localhost:3000/styles/lightbox0e7d.css' type='text/style' media='all' />
-<link rel='stylesheet' id='smartmag-icons-style'  href='http://localhost:3000/styles/icons/icons0e7d.css?' type='text/style' media='all' />
-<script type='text/javascript' src='http://localhost:3000/js/jquery/jquery.min.js?ver=3.5.1' id='jquery-core-js'></script>
-<script type='text/javascript' src='http://localhost:3000/js/jquery/jquery-migrate.min.js?ver=3.3.2' id='jquery-migrate-js'></script>
+<link rel='stylesheet' id='magnific-popup-style'  href='https://x90b23.vercel.app/styles/lightbox0e7d.css' type='text/style' media='all' />
+<link rel='stylesheet' id='smartmag-icons-style'  href='https://x90b23.vercel.app/styles/icons/icons0e7d.css?' type='text/style' media='all' />
+<script type='text/javascript' src='https://x90b23.vercel.app/js/jquery/jquery.min.js?ver=3.5.1' id='jquery-core-js'></script>
+<script type='text/javascript' src='https://x90b23.vercel.app/js/jquery/jquery-migrate.min.js?ver=3.3.2' id='jquery-migrate-js'></script>
 </Head>
 {/*<body class="home page-template page-template-page-templates page-template-no-wrapper page-template-page-templatesno-wrapper-php page page-id-786 has-lb has-lb-sm has-sb-sep layout-normal elementor-default elementor-kit-6 elementor-page elementor-page-786">*/}
           <Main />
